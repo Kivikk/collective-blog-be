03059ac799954cf0c6b95b40965151268e18a950
Yevhen <eugene.shust@gmail.com>
2024-11-21 17:12:56 +0100

    Temp code: create user for every Post

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,15 +14,28 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
     try {
-        const {
-            body: { title, content, cover },
-        } = req;
-        if (!title || !content || !cover)
+        //__________________TEMP______________________
+        const userCount = await User.count();
+        console.log(userCount);
+        const user = await User.create({
+            firstName: `firstName${userCount}`,
+            lastName: `lastName${userCount}`,
+            email: `email${userCount}`,
+        });
+        console.log(user.id);
+        //__________________TEMP______________________
+        const post = req.body;
+        post.author = user.id;
+        
+        console.log(post);
+        if (!post.title || !post.content || !post.cover)
             return res
                 .status(400)
                 .json({ error: 'Title, content, and cover are required' });
-        const post = await Post.create(req.body);
-        res.json(post);
+
+        const newPost = await Post.create(post);
+
+        res.json(newPost);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
